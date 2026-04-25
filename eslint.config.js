0c97b87a3beacc547a52578beaed0706a5b55661
Vicky Lin <vicky85412@gmail.com>
2026-04-25 16:32:29 +0800

    add eslint prettier config

---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+import js from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+import vueParser from 'vue-eslint-parser'
+import unusedImports from 'eslint-plugin-unused-imports'
+
+export default [
+  {
+    ignores: ['dist/**', '.quasar/**', 'node_modules/**'],
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+
+  {
+    files: ['**/*.{js,vue}'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      // Keep signal high, noise low for assignment speed
+      'no-console': 'off',
+
+      // Vue SFC ergonomics
+      'vue/multi-word-component-names': 'off',
+
+      // Prettier as ESLint rule
+      'prettier/prettier': 'warn',
+
+      // Simple Import Sort
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+
+      // Unused Imports
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': ['error', { vars: 'all', args: 'none' }],
+    },
+  },
+  prettierConfig,
+]
