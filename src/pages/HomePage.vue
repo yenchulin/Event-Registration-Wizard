a4c23220d1c4c0d093b37d04a00e05529e238498
Vicky Lin <vicky85412@gmail.com>
2026-04-29 22:36:33 +0800

    add home page ui

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,17 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import BaseButton from '@/components/wizard/BaseButton.vue'
+
+const router = useRouter()
+
+function goToRegistrationWizard() {
+  router.push({ name: 'registration-wizard' })
+}
+</script>
+
 <template>
-  <q-page><span>Home</span></q-page>
+  <q-page class="flex flex-col gap-4 items-center justify-center bg-brand-emphasis-rest">
+    <span class="text-h1 text-inverse">Welcome!</span>
+    <base-button @click="goToRegistrationWizard">Register</base-button>
+  </q-page>
 </template>
